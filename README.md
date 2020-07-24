dccfeef0590dbaf2b6dab994b448899f33684959
Christian Kaptur <kaptur@inmediasp.de>
2020-07-24 21:58:34 +0200

    Hello World example

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# evluation
+# evaluation
---
+++ src/main/java/com/evaluation/service/controller/EvaluationServiceController.java
@@ -0,0 +1,13 @@
+package com.evaluation.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EvaluationServiceController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
